ca7daa62cfdc0b357099c97a73724a8409858800
Bailey Day <bailey.j.day@gmail.com>
2020-03-25 11:14:14 -0400

    Fixed double selector problem, added click handling.

--- src/memorycard.jsx
+++ src/memorycard.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './MemoryCard.css';
 
+
+
 class MemoryCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
     render() {
-        return <div className="MemoryCard">
+        return <div className="MemoryCard" onClick={clickHandler}>
             <div className="MemoryCardInner">
                 <div className="MemoryCardBack">
                     <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="" />
@@ -14,6 +20,11 @@ class MemoryCard extends React.Component {
             </div>
         </div>
     }
+
 }
+function clickHandler() {
+    alert("Card Clicked")
+}
+
 
 export default MemoryCard;
